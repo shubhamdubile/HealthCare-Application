c1e8174e1843904b34254b9b714b1daddb3439bf
shubhamdubile <66626803+shubhamdubile@users.noreply.github.com>
2022-09-22 10:34:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ import re
 import numpy as np
 
 # Keras
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-import cv2
+# from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+# from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing import image
+# import cv2
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
@@ -28,11 +28,11 @@ import pickle
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
-MODEL_PATH ='new_vgg19_20sep.h5'
+# MODEL_PATH ='new_vgg19_20sep.h5'
 
 
 # Load your trained model
-model = load_model(MODEL_PATH)
+#model = load_model(MODEL_PATH)mt
 
 
 
@@ -42,34 +42,31 @@ model1 = pickle.load(open("RandomForestClassifier_10.pkl", 'rb'))
 
 
 
-def model_predict(img_path, model):
-    image = cv2.imread(img_path,1)
+# def model_predict(img_path, model):
+#     image = cv2.imread(img_path,1)
 
-    # Preprocessing the image
-    img = cv2.resize(image, (224,224),
-               interpolation = cv2.COLOR_RGB2BGR)
-    # x = np.true_divide(x, 255)
-    ## Scaling
-    x = img.reshape(1,224,224,3)
+    
+#     img = cv2.resize(image, (224,224),
+#                interpolation = cv2.COLOR_RGB2BGR)
+  
+#     x = img.reshape(1,224,224,3)
    
 
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    #x = preprocess_input(x)
+   
 
-    preds = model.predict(x)
-    preds=np.argmax(preds, axis=1)
-    if preds==0:
-        preds="The Person is Infected With glioma "
-    elif preds == 1:
-        preds="The Person Has NO_tumor"
-    elif preds == 2:
-        preds = "The person is Menginioun"
-    elif preds ==3:
-        preds = "The person is Pituratry"
+#     preds = model.predict(x)
+#     preds=np.argmax(preds, axis=1)
+#     if preds==0:
+#         preds="The Person is Infected With glioma "
+#     elif preds == 1:
+#         preds="The Person Has NO_tumor"
+#     elif preds == 2:
+#         preds = "The person is Menginioun"
+#     elif preds ==3:
+#         preds = "The person is Pituratry"
     
     
-    return preds
+#     return preds
 
 @app.route('/', methods=['GET'])
 def home():
@@ -81,29 +78,29 @@ def heart():
     # Main page
     return render_template('heart.html')
 
-@app.route('/index', methods=['GET'])
-def index():
-    # Main page
-    return render_template('index.html')
+# @app.route('/index', methods=['GET'])
+# def index():
+#     # Main page
+#     return render_template('index.html')
 
 
-@app.route('/predict', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
+# @app.route('/predict', methods=['GET', 'POST'])
+# def upload():
+#     if request.method == 'POST':
+#         # Get the file from post request
+#         f = request.files['file']
 
-        # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
+#         # Save the file to ./uploads
+#         basepath = os.path.dirname(__file__)
+#         file_path = os.path.join(
+#             basepath, 'uploads', secure_filename(f.filename))
+#         f.save(file_path)
 
-        # Make prediction
-        preds = model_predict(file_path, model)
-        result=preds
-        return result
-    return None
+#         # Make prediction
+#         preds = model_predict(file_path, model)
+#         result=preds
+#         return result
+#     return None
 
 
 @app.route('/result',methods=['GET','POST'])
@@ -274,5 +271,4 @@ def home1():
 
 
 if __name__ == '__main__':
-    #main()
     app.run(debug=True)
